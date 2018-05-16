8460900c6da1ac77fc78ca098c4b1669b8f369df
andrewlete92 <andrewlete@gmail.com>
2018-05-16 07:28:35 -0500

    Cambios 16-05-2018 07:28

--- Application/Db_Admin/Report.php
+++ Application/Db_Admin/Report.php
@@ -2,17 +2,46 @@
 require 'Conection.php';
 
 if($_POST['action']=='insert'){
-	$cod = $_POST['idEdit'];
-	$name = $_POST['name'];
-	$dir = $_POST['Address'];
-	$tel = $_POST['tel'];
+	$idCliente = 111;
+	$User = 'andres.angulo';
+	$fecha = 'CURRENT_DATE()';
+	$Placa = $_POST['Placa'];
+	$TipoVehiculo = $_POST['TipoVehiculo'];
+	$idCondicion = $_POST['idCondicion'];
+	$idDesperfecto = $_POST['idDesperfecto'];
+	$idSitCarretera = $_POST['idSitCarretera'];
+	$idCausa_Peaton = $_POST['idCausa_Peaton'];
+	$idCaract_Avenidas = $_POST['idCaract_Avenidas'];
+	$idCausa_Conductor = $_POST['idCausa_Conductor'];
+	$Conductor  = $_POST['Conductor'];
+	$Testimonio = $_POST['Testimonio'];
+	$Heridos = $_POST['Heridos'];
+	$Muertos = $_POST['Muertos'];
 
-	$sql = "INSERT INTO tbleditorial VALUES ('$cod', '$name', '$dir', '$tel')";
+	$sql = "INSERT INTO tblreportes (idCliente,Usuario, Fecha,Placa, TipoVehiculo, idCondicion, idDesperfecto, idSitCarretera,idCaract_Avenidas, idCausa_Peaton, idCausa_Conductor, Conductor, Testimonio)
+	VALUES
+	('$idCliente'
+	,'$User'
+	,'$fecha'
+	,'$Placa'
+	,'$TipoVehiculo'
+	,'$idCondicion'
+	,'$idDesperfecto'
+	,'$idSitCarretera'
+	,'$idCausa_Peaton'
+	,'$idCaract_Avenidas'
+	,'$idCausa_Conductor'
+	,'$Heridos'
+	,'$Muertos'
+	,'$Conductor'
+	,'$Testimonio'
+	)";
 	if(mySqli_query($mysqli,$sql)){
 		echo json_encode(array('answ' =>false));
 	} else {
 		echo json_encode(array('answ' =>true));
 	}
 } 
+
 $mysqli->close();
 ?>
--- js/Reportar.js
+++ js/Reportar.js
@@ -1,36 +1,59 @@
 $("#btnAction").on('click', function(event){
       event.preventDefault();
+      //alert("prueba");
       insertReport();
 })
 
 function insertReport(){
+	var idCondicion = $('#CondiAmbientales').val();
+	var idDesperfecto = $('#Desperfectos').val();
+	var idSitCarretera = $('#SCarreteras').val();
+	var idCausa_Peaton = $('#CausaPeaton').val();
+	var idCaract_Avenidas = $('#C_Avenidas').val();
+	var idCausa_Conductor = $('#CausaConductor').val();
+	var TipoVehiculo = $('#TipoVehiculo').val();
+	var heridos = $('#Heridos').val();
+	var muertos = $('#Muertos').val();
+	//var Conductor = $('#Conductor').val();
+	//var testimonio = $('#testimonio').val();
 	jQuery.ajax({
 		url: '../db_admin/Reporte.php',
 		type: 'POST',
 		dataType: 'json',
-		data: $('#formReport').serialize() + "&action=" + 'insert',
+		data: $('#formReport').serialize() + "&action=" + 'insert' +
+		"&placa=" + Placa +
+		"&idCondicion=" + idCondicion +
+		"&idDesperfecto=" + idDesperfecto +
+		"&idSitCarretera=" + idSitCarretera +
+		"&idCausa_Peaton=" + idCausa_Peaton +
+		"&idCaract_Avenidas=" + idCaract_Avenidas +
+		"&idCausa_Conductor=" + idCausa_Conductor +
+		"&TipoVehiculo=" + TipoVehiculo +
+		"&heridos=" + heridos +
+		"&Conductor=" + Conductor +
+		"&testimonio=" + testimonio +,
 	})
 	.done(function(data){
 		console.log("success");
 		console.log(data);
-		if(!data.answ){
+		/*if(!data.answ){
 			$("#modal").load('../Comun/Modals/Success_Save.html');
 			setTimeout(function(){
 			$(".modalpopup").modal();
 			},500);
-		}
+		}*/
+		//limpiar();
 	})
 	.fail(function(fail) {
 		console.log('fail');
-		$("#modal").load('../Comun/Modals/Error_Save.html');
+		/*$("#modal").load('../Comun/Modals/Error_Save.html');
 		setTimeout(function(){
 		$(".modalpopup").modal();
-		},500);
+		},500);*/
 	})
 	.always(function() {
 		console.log("complete");
-		limpiar();
-	});	
+	});
 }
 
 function limpiar(){
